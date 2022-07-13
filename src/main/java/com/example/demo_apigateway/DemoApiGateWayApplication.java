34f575622b895a0323943a1ce68e9041b3c0bdf4
HoangKien <hoangkien@gmail.com>
2022-07-13 23:26:53 +0700

    HoangKien

---
+++ src/main/java/com/example/demo_apigateway/DemoApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiGateWayApplication.class, args);
+    }
+
+}
